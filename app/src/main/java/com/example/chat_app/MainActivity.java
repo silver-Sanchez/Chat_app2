0c7b34853a6ef0e07398040438ff38781ecfcca3
silver-Sanchez <concretemixer@yandex.ru>
2021-11-30 18:57:26 +0300

    v3

--- app/src/main/java/com/example/chat_app/MainActivity.java
+++ app/src/main/java/com/example/chat_app/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.chat_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int  SIGN_IN_CODE = 1;
+    private RelativeLayout activity_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activity_main = findViewById(R.id.activity_main); // функция позволяет находить айди объекта
+
+        //если пользователь ещё не авторизован
+        if (FirebaseAuth.getInstance().getCurrentUser() == null)
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE);
+        else
+            Snackbar.make().show();
     }
 }
